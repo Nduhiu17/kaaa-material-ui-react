f845a3e18b89378afff2752d83fa992dc18a53e3
Nduhiu17 <nduhiu254@gmail.com>
2020-08-02 16:23:52 +0300

    design main slider

---
+++ src/components/main-slider/MainSlider.js
@@ -0,0 +1,63 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+import Carousel from 'react-material-ui-carousel'
+import {Paper, Toolbar, Typography} from '@material-ui/core'
+
+//images
+import imgOne from '../../assets/slider/1.jpg'
+import imgTwo from '../../assets/slider/2.jpg'
+import imgThree from '../../assets/slider/3.jpg'
+import imgFour from '../../assets/slider/4.jpg'
+import imgFive from '../../assets/slider/5.jpg'
+import imgSix from '../../assets/slider/6.jpeg'
+import imgSeven from '../../assets/slider/7.jpeg'
+import imgEight from '../../assets/slider/12.jpg'
+ 
+export default function MainSlider(props)
+{
+    var items = [
+        {
+            image: <img src = {imgOne} alt="image one" className={""}/>
+        },
+        {
+            image: <img src = {imgTwo} alt="image one"/>
+        },
+        {
+            image: <img src = {imgThree} alt="image one"/>
+        },
+        {
+            image: <img src = {imgFour} alt="image one"/>
+        },
+        {
+            image: <img src = {imgFive} alt="image one"/>
+        },
+        {
+            image: <img src = {imgSix} alt="image one"/>
+        },
+        {
+            image: <img src = {imgSeven} alt="image one"/>
+        },
+        {
+            image: <img src = {imgEight} alt="image one"/>
+        }
+    ]
+ 
+    return (
+        <Carousel>
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
+    )
+}
+ 
+function Item(props)
+{
+    return (
+        <Typography>
+            <div className="image-wrapper">
+                {props.item.image}
+            </div>
+        </Typography>
+    )
+}
