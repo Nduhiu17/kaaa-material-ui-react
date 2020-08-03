926401c9e4cd1b7f5c14e386d2d28b5c7d79c36b
Nduhiu17 <nduhiu254@gmail.com>
2020-08-03 09:50:26 +0300

    update css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon  from '@material-ui/icons/Menu';
+import Logo from '../assets/img/KAAA_Colour Logo.jpg'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,6 +19,10 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    Logo: {
+      width:80,
+      height:45
+    }
   }),
 );
 
@@ -28,13 +33,18 @@ export default function Header() {
     <div className={classes.root}>
       <AppBar position="static" color="transparent">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
+          </IconButton> */}
+           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+           <img src={  Logo } alt='kaaa' className={classes.Logo}/>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             KAAA
           </Typography>
-          <Button color="inherit">Contacts</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Membership</Button>
+          <Button color="inherit">Gallery</Button>
         </Toolbar>
       </AppBar>
     </div>
