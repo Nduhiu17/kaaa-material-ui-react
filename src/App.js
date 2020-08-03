f155789e135cf3fc0045e56642a62f141c584af9
Nduhiu17 <nduhiu254@gmail.com>
2020-08-03 10:15:02 +0300

    change appbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { Paper } from '@material-ui/core';
 import Header from './components/Header'
-import MainSlider from './components/sliders/MainSlider'
-import About from './components/About'
-import Objectives from './components/Objectives'
-import EventsAndMembership from './components/EventsAndMembership'
-import Main from './components/gallery/Main'
-import Footer from './components/Footer'
+
 
 function App() {
   return (
     <Paper elevation={10}>
       <Header/>
-      <MainSlider />
-      <About/>
-      <Objectives />
-      <EventsAndMembership />
-      <Main />
-      <Footer/>
     </Paper>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,53 +1,95 @@
-import React from 'react'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon  from '@material-ui/icons/Menu';
-import Logo from '../assets/img/KAAA_Colour Logo.jpg'
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    Logo: {
-      width:80,
-      height:45
-    }
-  }),
-);
+import MainSlider from './sliders/MainSlider'
+import About from './About'
+import Objectives from './Objectives'
+import EventsAndMembership from './EventsAndMembership'
+import Main from './gallery/Main'
+import Footer from './Footer'
 
-export default function Header() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children, window } = props;
   const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="transparent">
-        <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
-           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-           <img src={  Logo } alt='kaaa' className={classes.Logo}/>
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            KAAA
-          </Typography>
-          <Button color="inherit">About</Button>
-          <Button color="inherit">Membership</Button>
-          <Button color="inherit">Gallery</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
   );
 }
 
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function Header(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">Scroll to see button</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      {/* <Container> */}
+        <Box my={2}>
+          <MainSlider />
+          <About/>
+          <Objectives />
+          <EventsAndMembership />
+          <Main />
+          <Footer/>
+        </Box>
+      {/* </Container> */}
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
+  );
+}
